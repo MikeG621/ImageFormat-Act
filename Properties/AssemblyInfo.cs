1c08e0e1e9c11fccca7994f938abbe5f680be005
Mike <mjgaisser@gmail.com>
2011-08-14 11:29:44 -0500

    initial upload

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,28 @@
+﻿/*
+ * Idmr.ImageFormat.Act, Allows editing capability of LucasArts *.ACT files.
+ * Copyright (C) 2009 Michael Gaisser (mjgaisser@gmail.com)
+ * 
+ * Full notice in Act.cs
+ */
+
+using System.Reflection;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Idmr.ImageFormat.Act")]
+[assembly: AssemblyDescription("Allows editing capability of LucasArts *.ACT files")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Imperial Department of Military Research")]
+[assembly: AssemblyProduct("Idmr.ImageFormat.Act")]
+[assembly: AssemblyCopyright("Copyright © Michael Gaisser 2009")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+[assembly: AssemblyVersion("1.0.2.*")]
